ff62687aa7f711a1a81acdefad8c0549515c3088
Marcus McKinnon <marcus.mckinnon@halliburton.com>
2022-01-05 13:44:50 -0600

    Updated to new version of Doom

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Fira Code" :size 20))
+(setq doom-font (font-spec :family "Fira Code Nerd Font" :weight 'light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -54,36 +54,4 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq org-journal-date-prefix "#+TITLE: "
-      org-journal-time-prefix "* "
-      org-journal-date-format "%a, %Y-%m-%d"
-      org-journal-file-format "%Y-%m-%d.org")
-
-;; magit and source
-(setq auth-sources '("~/.authinfo"))
-(setq github.user "zhuum"
-      github.HOST.user "zhuum")
-
-(setq delete-by-moving-to-trash t)
-
-;; IRC config
-(setq erc-server "irc.libera.chat"
-        erc-nick "Marcuus"     ; Our IRC nick
-        erc-user-full-name "Marcus McKinnon" ; Our /whois name
-        erc-track-shorten-start 8
-        erc-autojoin-channels-alist '(("#systemcrafters"))
-        erc-kill-buffer-on-part t
-        erc-auto-query 'bury)
-
-(setq erc-fill-column 120
-      erc-fill-function 'erc-fill-static
-      erc-fill-static-center 30)
-
-(setq erc-track-exclude '("irc.libera.chat" "#systemcrafters")
-      erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "AWAY")
-      erc-hide-list '("JOIN" "NICK" "PART" "QUIT" "MODE" "AWAY")
-      erc-track-exclude-server-buffer t)
-
-(setq org-roam-directory "~/org/roam")
-
-(setq python-black-on-save-mode t)
+(setq scroll-margin 8)
--- packages.el
+++ packages.el
@@ -49,4 +49,4 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 
-(package! python-black)
+(package! coffee-mode)
